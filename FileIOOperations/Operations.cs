0dbbfddfaab7a10c26b6e02f6c989bfa27232895
unknown <sivaranjanib99@gmail.com>
2021-07-26 15:13:46 +0530

    [siva] basic operations in IO file

---
+++ FileIOOperations/Operations.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FileIOOperations
+{
+    class Operations
+    {
+        //method to check existance of a file
+        public static void FileExits(String filePath)
+        {
+            if(File.Exists(filePath))
+            {
+                Console.WriteLine("file exists");
+            }
+            else
+            {
+                Console.WriteLine("file not found");
+            }
+
+        }
+        public static void ReadAllLines(String filePath)
+        {
+            string[] lines;
+            lines = File.ReadAllLines(filePath);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                Console.WriteLine(lines[i]);
+            }
+        }
+        public static void ReadAllText(string filePath)
+        {
+            string text;
+            text = File.ReadAllText(filePath);
+            Console.WriteLine("{0}", text);
+        }
+        public static void FileCopy(string InputFile, string OutputFile )
+        {
+            File.Copy(InputFile, OutputFile);
+            Console.WriteLine("File is copied");
+        }
+        public static void DeleteFile(string filePath)
+        {
+            File.Delete(filePath);
+            Console.WriteLine("the file has been deleted");
+        }
+        public static void ReadfromStreamReader(string filePath)
+        {
+            StreamReader sr = File.OpenText(filePath);
+            string text = "";
+            while((text = sr.ReadLine())!=null)
+            {
+                Console.WriteLine(text);
+            }
+        }
+        public static void WriteusingStreamWriter(string filePath)
+        {
+            using (StreamWriter sr = File.AppendText(filePath))
+            {
+               
+                sr.WriteLine("happy to learn");
+                sr.Close();
+            }
+            Console.WriteLine(File.ReadAllText(filePath));
+        }
+    }
+}
---
+++ FileIOOperations/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FileIOOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to File IOOperation!!");
+            String filePath = @"C:\Users\user\source\repos\FileIOOperations\FileIOOperations\textfile.txt";
+            string OutputPath = @"C:\Users\user\source\repos\FileIOOperations\FileIOOperations\textfilee1.txt";
+            Operations.FileExits(filePath);
+            Operations.ReadAllLines(filePath);
+           // Operations.ReadAllText(filePath);
+            //Operations.ReadfromStreamReader(filePath);
+            Operations.WriteusingStreamWriter(filePath);
+            Console.Read();
+        }
+    }
+}
